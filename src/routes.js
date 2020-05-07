3d9423fb73965262d9b48b54f7ef8702d9c7b06b
paulodelia <paulohdelia@gmail.com>
2020-05-06 22:24:40 -0300

    feat: update and delete products

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,11 @@ routes.get('/', function(req, res){
 });
 
 routes.get('/products/create', ProductController.create);
-routes.post('/products', ProductController.post)
+routes.get('/products/:id/edit', ProductController.edit);
+
+routes.post('/products', ProductController.post);
+routes.put('/products', ProductController.put);
+routes.delete('/products', ProductController.delete);
 
 
 // Alias
