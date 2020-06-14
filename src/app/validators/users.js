5261a116d4ced4c689eb320cc195b0c9b754b856
paulodelia <paulohdelia@gmail.com>
2020-06-13 22:20:02 -0300

    feat: custom error message

--- src/app/validators/users.js
+++ src/app/validators/users.js
@@ -1,29 +1,40 @@
 const User = require('../models/User');
 
-module.exports = {
-    async post(req, res, next) {
-        const keys = Object.keys(req.body);
-    
-            for (key of keys) {
-                if(req.body[key] == '') {
-                    return res.send('Please, fill all fields!');
-                }
+async function post(req, res, next) {
+    const keys = Object.keys(req.body);
+
+        for (key of keys) {
+            if(req.body[key] == '') {
+                return res.render('user/register', {
+                    user: req.body,
+                    error: 'Por favor, preencha todos os campos!'
+                });
             }
-    
-            const { email, cpf_cnpj, password, passwordRepeat } = req.body;
-            const user = await User.findOne({
-                where: {email},
-                or: {cpf_cnpj} 
+        }
+
+        const { email, cpf_cnpj, password, passwordRepeat } = req.body;
+        const user = await User.findOne({
+            where: {email},
+            or: {cpf_cnpj} 
+        });
+
+        if (user) {
+            return res.render('user/register', {
+                user: req.body,
+                error: 'Usuário já cadastrado.'
             });
-    
-            if (user) {
-                return res.send('Users exists')
-            }
-    
-            if (password != passwordRepeat) {
-                return res.send('Password mismatch');
-            }
-    
-            next();
-    }
+        }
+
+        if (password != passwordRepeat) {
+            return res.render('user/register', {
+                user: req.body,
+                error: 'As senhas estão diferentes.'
+            });
+        }
+
+        next();
+}
+
+module.exports = {
+    post
 }
