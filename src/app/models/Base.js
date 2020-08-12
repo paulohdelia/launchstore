aaa7472fd123a6ae4bbf837944d684c97efc9736
paulohdelia <paulohdelia@gmail.com>
2020-08-12 13:58:06 -0300

    fix: corret errors at create in the Base model
    The values were not receiving strings and the return was results.row

--- src/app/models/Base.js
+++ src/app/models/Base.js
@@ -44,7 +44,7 @@ const Base = {
 
       Object.keys(fields).map(key => {
         keys.push(key);
-        values.push(fields[key]);
+        values.push(`'${fields[key]}'`);
       });
 
       const query = `INSERT INTO ${this.table} (${keys.join(',')}) 
@@ -53,7 +53,7 @@ const Base = {
       `;
 
       const results = await db.query(query);
-      return results.row[0].id;
+      return results.rows[0].id;
     } catch (error) {
       console.error(error);
     }
