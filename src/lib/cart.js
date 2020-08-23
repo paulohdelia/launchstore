ec28b5dbef06eb1096fc06a5070bbd77d2cf3325
paulohdelia <paulohdelia@gmail.com>
2020-08-22 23:21:08 -0300

    feat: add item to cart

--- src/lib/cart.js
+++ src/lib/cart.js
@@ -16,9 +16,53 @@ const Cart = {
 
     return this;
   },
-  addOne(product) { },
+  addOne(product) {
+    let inCart = this.items.find(item => item.product.id == product.id);
+
+    if (!inCart) {
+      inCart = {
+        product: {
+          ...product,
+          formattedPrice: formatPrice(product.price),
+        },
+        quantity: 0,
+        price: 0,
+        formattedPrice: formatPrice(0),
+      };
+
+      this.items.push(inCart)
+    }
+
+    if (inCart.quantity >= product.quantity) return this;
+
+    inCart.quantity++;
+    inCart.price = inCart.product.price * inCart.quantity;
+    inCart.formattedPrice = formatPrice(inCart.price);
+
+    this.total.quantity++;
+    this.total.price += inCart.product.price;
+    this.total.formattedPrice = formatPrice(this.total.price);
+
+    return this
+  },
   removeOne(productId) { },
   delete(productId) { },
 }
+
+const product = {
+  id: 1,
+  price: 199,
+  quantity: 2
+}
+
+const product2 = {
+  id: 2,
+  price: 5,
+  quantity: 30
+}
+
+
+console.log(Cart.init().addOne(product).addOne(product).addOne(product).addOne(product2).addOne(product).addOne(product2));
+
 
 module.exports = Cart;
