862c66b9f140c26918ffa937b0a78419579bc4e7
paulohdelia <paulohdelia@gmail.com>
2020-09-06 23:04:10 -0300

    feat: add items to cart

--- src/app/controllers/CartController.js
+++ src/app/controllers/CartController.js
@@ -1,18 +1,31 @@
-const Cart = require('../../lib/cart');
+const { addOne } = require("../../lib/cart");
+const Cart = require("../../lib/cart");
 
-const LoadProductsService = require('../services/LoadProducts');
+const LoadProductsService = require("../services/LoadProducts");
 
 module.exports = {
-    async index(req, res) {
-        try {
-            const product = await LoadProductsService.load('product', { where: { id: 1 } });
-            let { cart } = req.session;
+  async index(req, res) {
+    try {
+      let { cart } = req.session;
 
-            cart = Cart.init(cart).addOne(product);
+      cart = Cart.init(cart);
 
-            return res.render('cart/index', { cart });
-        } catch (error) {
-            console.error(error);
-        }
+      return res.render("cart/index", { cart });
+    } catch (error) {
+      console.error(error);
     }
-}
+  },
+  async addOne(req, res) {
+    const { id } = req.params;
+
+    const product = await LoadProductsService.load("product", {
+      where: { id },
+    });
+
+    const { cart } = req.session;
+
+    req.session.cart = Cart.init(cart).addOne(product);
+
+    return res.redirect("/cart");
+  },
+};
--- src/routes/cart.js
+++ src/routes/cart.js
@@ -1,8 +1,9 @@
-const express = require('express');
+const express = require("express");
 const routes = express.Router();
 
-const CartController = require('../app/controllers/CartController');
+const CartController = require("../app/controllers/CartController");
 
-routes.get('/', CartController.index)
+routes.get("/", CartController.index);
+routes.post("/:id/add-one", CartController.addOne);
 
 module.exports = routes;
